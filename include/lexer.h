7fd2ee808c4074e370584f65a7f60399b6dfe3d1
Jasmine Djama Masopeh <masopeh@linprog6.cs.fsu.edu>
2023-10-07 20:47:54 -0400

    angela's changes to part 8 with part 9

--- include/lexer.h
+++ include/lexer.h
@@ -2,6 +2,7 @@
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <sys/types.h>
 
 typedef struct {
     char ** items;
@@ -11,7 +12,8 @@ typedef struct {
 char * environmentVariables(tokenlist * tokens);
 char * tildeExpansion(tokenlist * tokens);
 char * pathSearch(tokenlist * tokens);
-char * ExternalCommandExec(const tokenlist * tokens, char * filePath);
+char * ExternalCommandExec(tokenlist * tokens, char * filePath);
+void ioRedirection(tokenlist * tokens);
 char *  piping(tokenlist * tokens);
 char * internalCommandExecution(tokenlist * tokens);
 void printList(tokenlist * tokens); 
@@ -19,7 +21,18 @@ void prompt();
 void lexer_parse_token(); 
 void historyCommandList(tokenlist * tokens, char * input);
 bool hasPipe(tokenlist * tokens);
+bool hasRedirector(tokenlist * tokens);
 void executeAllCommands(tokenlist * tokens, char * input);
+
+void BackgroundProcess(tokenlist * tokens, int JOB_NUMBER, bool has_pipe, bool has_redirector);
+void storeBackgroundProcessInfo(int jobNumber, pid_t pid, const char *commandLine);
+pid_t getProcessPID(int jobNumber);
+const char *getProcessCommandLine(int jobNumber);
+bool isProcessActive(int jobNumber);
+void listActiveBackgroundProcesses();
+void printActiveBackgroundProcesses();
+int backgroundProcessesCount();
+
 
 char * get_input(void);
 tokenlist * get_tokens(char *input);
--- src/lexer.c
+++ src/lexer.c
@@ -5,21 +5,32 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include "mytimeout.h"
+
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #define MAX_HISTORY_SIZE 3
+int JOB_NUMBER = 0;
 
 struct commandHistory {
     char commands[MAX_HISTORY_SIZE][200]; // Assuming commands are less than 200 characters
     int count;
 };
 
+struct backgroundProcesses 
+{
+	int jobNumber;
+    pid_t pid; //process identification. used to represent process ids.
+    char commandLine[200];
+};
+
 struct commandHistory history;
+// Define a global array to store background process information
+struct backgroundProcesses backgroundProcessList[MAX_HISTORY_SIZE];
 
 int main()
 {
 	history.count = 0;
-
 	lexer_parse_token();
 
 	return 0;
@@ -47,34 +58,93 @@ void lexer_parse_token()
 
 void executeAllCommands(tokenlist * tokens, char * input)
 {
-	// while(1)
-	// {
-		if (tokens->size > 0) 
+
+	if (tokens->size > 0) 
+	{
+		historyCommandList(tokens, input);
+
+		/*
+		// Check for input or output redirection
+		int input_redirection = -1;  // Index of "<" token
+		int output_redirection = -1; // Index of ">" token
+
+		for (int i = 0; i < tokens->size; i++)
 		{
-			historyCommandList(tokens, input);
-			if (hasPipe(tokens)) 
+			if (strcmp(tokens->items[i], "<") == 0)
 			{
-				piping(tokens);
+				input_redirection = i;
 			}
-			else 
+			else if (strcmp(tokens->items[i], ">") == 0)
 			{
-				if (strcmp(tokens->items[0],"exit") == 0 || strcmp(tokens->items[0],"cd") == 0)
-				{
-					// running inernal command execution
-					internalCommandExecution(tokens);			
-				}
-				else
-				{
-					tokens->items[1] = environmentVariables(tokens); // what happens to previous memory in token->items[1]?
-					tokens->items[1] = tildeExpansion(tokens);
-					tokens->items[0] = pathSearch(tokens);
-					ExternalCommandExec(tokens, tokens->items[0]);
-				}
+				output_redirection = i;
 			}
 		}
-		// print tokens
-		printList(tokens);
-	// }
+			
+		
+	
+		// Call ioRedirection function to handle redirection
+		if (input_redirection != -1 || output_redirection != -1)
+		{
+			ioRedirection(tokens);
+		}
+		*/
+
+		
+		// int has_redirector = 0;
+		// for (size_t i = 0; i < tokens->size; i++) 
+		// {
+		// 	if (strcmp(tokens->items[i], "<") == 0 || strcmp(tokens->items[i], ">") == 0) 
+		// 	{
+		// 		has_redirector = 1;
+		// 		break;
+		// 	}
+		// }
+		// if (has_redirector) 
+		// {
+		// 	// Execute piped commands
+		// 	ioRedirection(tokens);
+		// }
+
+
+		//check if last token is &
+		if (strcmp(tokens->items[tokens->size - 1], "&") == 0) {
+			//background commands
+			tokenlist * tokens2 = {NULL};
+			tokens2 = new_tokenlist();
+			for (int i = 0; i < tokens->size - 1; i++) //removes & as a token
+			{
+				add_token(tokens2, tokens->items[i]);
+			}
+			JOB_NUMBER++;
+			BackgroundProcess(tokens2, JOB_NUMBER, hasPipe(tokens), hasRedirector(tokens));
+		}
+		else if (hasPipe(tokens)) 
+		{
+			piping(tokens);
+		}
+		else if (hasRedirector(tokens))
+		{
+			ioRedirection(tokens);
+		}
+		else 
+		{
+			if (strcmp(tokens->items[0],"exit") == 0 || strcmp(tokens->items[0],"cd") == 0 || strcmp(tokens->items[0],"jobs") == 0 )
+			{
+				// running inernal command execution
+				internalCommandExecution(tokens);			
+			}
+			else
+			{
+				tokens->items[1] = environmentVariables(tokens); // what happens to previous memory in token->items[1]?
+				tokens->items[1] = tildeExpansion(tokens);
+				tokens->items[0] = pathSearch(tokens);
+				ExternalCommandExec(tokens, tokens->items[0]);
+			}
+		}
+	}
+	// print tokens
+	printList(tokens);
+
 }
 
 void historyCommandList(tokenlist * tokens, char * input )
@@ -112,6 +182,24 @@ bool hasPipe(tokenlist * tokens)
 			// has_pipe = 1;
 			hasPipe = true;
 			return hasPipe;
+			break;
+		}
+	}
+	return false; 
+}
+
+bool hasRedirector(tokenlist * tokens)
+{
+	// Check if the input contains a pipe character
+	// int has_pipe = 0;
+	bool hasRedirector = false;
+	for (size_t i = 0; i < tokens->size; i++) 
+	{
+		if (strcmp(tokens->items[i], "|") == 0) 
+		{
+			// has_pipe = 1;
+			hasRedirector = true;
+			return hasRedirector;
 			break;
 		}
 	}
@@ -186,8 +274,6 @@ char * tildeExpansion(tokenlist *tokens)
 	return tokens->items[1];
 }
 
-// https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
-
 char * pathSearch(tokenlist * tokens)
 {
    char * fullPath = malloc(sizeof(char) * strlen(getenv("PATH")));
@@ -230,7 +316,96 @@ char * pathSearch(tokenlist * tokens)
 		return filePath; 
 }
 
-char * ExternalCommandExec(const tokenlist * tokens, char * filePath)
+void ioRedirection(tokenlist *tokens)
+{
+    int input_redirection = -1;  // File descriptor for input redirection
+    int output_redirection = -1; // File descriptor for output redirection
+
+    // Check for input redirection
+    for (int i = 0; i < tokens->size; i++)
+    {
+        if (strcmp(tokens->items[i], "<") == 0)
+        {
+            input_redirection = i; // Store the index of the "<" token
+            break;
+        }
+    }
+
+    // Check for output redirection
+    for (int i = 0; i < tokens->size; i++)
+    {
+        if (strcmp(tokens->items[i], ">") == 0)
+        {
+            output_redirection = i; // Store the index of the ">" token
+            break;
+        }
+    }
+
+    if (input_redirection != -1)
+    {
+        // Handle input redirection
+        if (input_redirection + 1 < tokens->size)
+        {
+            char *input_file = tokens->items[input_redirection + 1];
+            // Open the input file for reading and replace standard input
+			close(0);
+            int fd = open(input_file, O_RDONLY);
+            if (fd == -1)
+            {
+                perror("open");
+                exit(EXIT_FAILURE);
+            }
+            // dup2(fd, STDIN_FILENO);
+            // close(fd);
+
+            // Remove the redirection tokens and the input file from the token list
+            for (int i = input_redirection; i < tokens->size - 2; i++)
+            {
+                tokens->items[i] = tokens->items[i + 2];
+            }
+            tokens->size -= 2;
+        }
+        else
+        {
+            fprintf(stderr, "Error: Missing input file for redirection.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (output_redirection != -1)
+    {
+        // Handle output redirection
+        if (output_redirection + 1 < tokens->size)
+        {
+            char *output_file = tokens->items[output_redirection + 1];
+            // Open the output file for writing and replace standard output'
+			close(1);
+            int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+            if (fd == -1)
+            {
+                perror("open");
+                exit(EXIT_FAILURE);
+            }
+            // dup2(fd, STDOUT_FILENO);
+            // close(fd);
+
+            // Remove the redirection tokens and the output file from the token list
+            for (int i = output_redirection; i < tokens->size - 2; i++)
+            {
+                tokens->items[i] = tokens->items[i + 2];
+            }
+            tokens->size -= 2;
+        }
+        else
+        {
+            fprintf(stderr, "Error: Missing output file for redirection.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+char * ExternalCommandExec(tokenlist * tokens, char * filePath)
 {
 	pid_t pid;
 	int status;
@@ -238,6 +413,22 @@ char * ExternalCommandExec(const tokenlist * tokens, char * filePath)
 	
 	if (pid == 0) // do only if child process
 	{
+		int has_redirector = 0;
+		for (size_t i = 0; i < tokens->size; i++) 
+		{
+			if (strcmp(tokens->items[i], "<") == 0 || strcmp(tokens->items[i], ">") == 0) 
+			{
+				has_redirector = 1;
+				break;
+			}
+		}
+		if (has_redirector) 
+		{
+			// Execute piped commands
+			ioRedirection(tokens);
+		}
+
+		// have to call redirection in here
 		char *argv[tokens->size + 1];
 		for (int i = 0; i < tokens->size; i++)
 		{
@@ -254,21 +445,21 @@ char * ExternalCommandExec(const tokenlist * tokens, char * filePath)
 
 char * piping(tokenlist *tokens) 
 {
-    size_t num_tokens = tokens->size;
+    size_t numTokens = tokens->size;
 
     // Find the positions of the pipe tokens ("|")
-    size_t pipe_positions[2] = {0};
-    int pipe_count = 0; // Use this becasue there could be one or two pipes
-    for (size_t i = 0; i < num_tokens; i++) 
+    size_t pipePositions[2] = {0};
+    int pipeCount = 0; // Use this becasue there could be one or two pipes
+    for (size_t i = 0; i < numTokens; i++) 
 	{
         if (strcmp(tokens->items[i], "|") == 0) 
 		{
-            pipe_positions[pipe_count] = i;
-            pipe_count++;
+            pipePositions[pipeCount] = i;
+            pipeCount++;
         }
     }
 	// pipe has to be more than one. Pipe cannot be located as the first character, and cannot be last character
-    if (pipe_count < 1 || pipe_positions[0] == 0 || pipe_positions[pipe_count - 1] == num_tokens - 1) 
+    if (pipeCount < 1 || pipePositions[0] == 0 || pipePositions[pipeCount - 1] == numTokens - 1) 
 	{
         fprintf(stderr, "Invalid input: Missing command(s)\n");
 		return 0; 
@@ -276,23 +467,23 @@ char * piping(tokenlist *tokens)
 
     // Create an array of tokenlists for commands (There can be two and three commands)
     tokenlist * commands[3] = {NULL};
-    for (int i = 0; i <= pipe_count; i++)  // based on the number of new tokens
+    for (int i = 0; i <= pipeCount; i++)  // based on the number of new tokens
 	{
         commands[i] = new_tokenlist();
     }
 
     // Populate the tokenlists for commands
     size_t start = 0; // 
-    for (int i = 0; i <= pipe_count; i++) 
+    for (int i = 0; i <= pipeCount; i++) 
 	{
         size_t end;
-		if (i == pipe_count)
+		if (i == pipeCount)
 		{
-			end	= num_tokens;
+			end	= numTokens;
 		}
 		else
 		{
-			end = pipe_positions[i];
+			end = pipePositions[i];
 		}
         for (size_t j = start; j < end; j++) 
 		{
@@ -306,7 +497,7 @@ char * piping(tokenlist *tokens)
     pid_t pids[3]; 
 
     // Create pipes
-    for (int i = 0; i < pipe_count; i++) 
+    for (int i = 0; i < pipeCount; i++) 
 	{
         if (pipe(pipefds[i]) == -1) 
 		{
@@ -315,7 +506,7 @@ char * piping(tokenlist *tokens)
         }
     }
 
-    for (int i = 0; i <= pipe_count; i++) 
+    for (int i = 0; i <= pipeCount; i++) 
 	{
         pids[i] = fork();
         if (pids[i] == -1) 
@@ -334,7 +525,7 @@ char * piping(tokenlist *tokens)
                 close(pipefds[i - 1][0]);
             }
 
-            if (i < pipe_count) 
+            if (i < pipeCount) 
 			{
                 // Redirect standard output to the current pipe
                 dup2(pipefds[i][1], STDOUT_FILENO);
@@ -342,18 +533,18 @@ char * piping(tokenlist *tokens)
             }
 
             // Close all pipe file descriptors
-            for (int j = 0; j < pipe_count; j++) 
+            for (int j = 0; j < pipeCount; j++) 
 			{
                 close(pipefds[j][0]);
                 close(pipefds[j][1]);
             }
 
             // Execute the command
-            char * cmd_path = pathSearch(commands[i]);
-            if (cmd_path) 
+            char * commandPath = pathSearch(commands[i]);
+            if (commandPath) 
 			{
-                ExternalCommandExec(commands[i], cmd_path);
-                free(cmd_path);
+                ExternalCommandExec(commands[i], commandPath);
+                free(commandPath);
             } 
 			else 
 			{
@@ -366,22 +557,23 @@ char * piping(tokenlist *tokens)
         }
     }
 	
-    // Parent process
-    // Close all pipe file descriptors
-    for (int j = 0; j < pipe_count; j++) 
+	/**
+	 * 
+	 * Close all pipe file descriptors
+	*/
+	// Parent process
+    for (int j = 0; j < pipeCount; j++) 
 	{
         close(pipefds[j][0]);
         close(pipefds[j][1]);
     }
-
     // Wait for all child processes to complete
-    for (int i = 0; i <= pipe_count; i++) 
+    for (int i = 0; i <= pipeCount; i++) 
 	{
         waitpid(pids[i], NULL, 0);
     }
-
     // Free tokenlists
-    for (int i = 0; i <= pipe_count; i++) 
+    for (int i = 0; i <= pipeCount; i++) 
 	{
         free_tokens(commands[i]);
     }
@@ -397,6 +589,7 @@ char * internalCommandExecution(tokenlist * tokens)
 		// Display the last three valid commands
         
 		int start; 
+
 		if (history.count > 3)
 		{
 			start = history.count - 3; // if we pass three commands then start from 1 again
@@ -423,6 +616,7 @@ char * internalCommandExecution(tokenlist * tokens)
 		exit(0);
 		printf("User wants to exit");
 	}
+
 	else if(strcmp(tokens->items[0], "cd") == 0)
 	{
 		// if there are no arguments supplied
@@ -444,10 +638,201 @@ char * internalCommandExecution(tokenlist * tokens)
 	}
 	else if (strcmp(tokens->items[0], "jobs") == 0)
 	{
-		
+		printf("Output a list of active processes\n");
+		listActiveBackgroundProcesses();
 	}
+
 	return tokens->items[1]; 
 }
+
+void BackgroundProcess(tokenlist* tokens, int JOB_NUMBER, bool has_pipe, bool has_redirector)
+{
+	if (has_pipe) {
+		int status;
+		pid_t PID = fork();
+		if (PID == 0){
+			printf("\n[ %d ] [ %d ]\n", JOB_NUMBER, getpid());
+			piping(tokens);
+			printf("[ %d ] + done [ ", JOB_NUMBER);
+			printList(tokens);
+			printf("]\n");
+			exit(0);
+		}
+		waitpid(PID, &status, WNOHANG);
+	}
+	else if (has_redirector) {
+		int status;
+		pid_t PID = fork();
+		if (PID != 0)
+		{
+			printf("[%d] [%d]\n", JOB_NUMBER, PID);
+			piping(tokens);
+			printf("[ %d ] + done [ ", JOB_NUMBER);
+			printList(tokens);
+			printf("]\n");
+			exit(0);
+		}
+		waitpid(PID, &status, WNOHANG);
+	}
+	else {
+		int status;
+		pid_t PID = fork();
+		
+		if (PID == 0) // do only if child process
+		{
+			printf("\n[ %d ] [ %d ]\n", JOB_NUMBER, getpid());
+			int status2;
+			pid_t pid2 = fork();
+			if (pid2 == 0) //child^2		
+			{	
+				char *argv[tokens->size + 1];
+				for (int i = 0; i < tokens->size; i++)
+				{
+					argv[i] = tokens->items[i];
+				}
+				argv[tokens->size] = NULL;
+
+				execv(pathSearch(tokens), argv);
+			}
+			waitpid(pid2, &status2, 0);
+			if (WIFEXITED(status2)) {
+				printf("[ %d ] + done [ ", JOB_NUMBER);
+				printList(tokens);
+				printf("]\n");
+			}
+		}
+		waitpid(PID, &status, WNOHANG); //don't wait for child process to finish
+	}
+}
+
+
+
+// Function to store the PID and command line of a background process
+void storeBackgroundProcessInfo(int jobNumber, pid_t pid, const char *commandLine)
+{
+    // Find an empty slot in the backgroundProcessList and store the information
+    for (int i = 0; i < MAX_HISTORY_SIZE; i++)
+    {
+        if (backgroundProcessList[i].pid == 0)
+        {
+            backgroundProcessList[i].jobNumber = jobNumber;
+            backgroundProcessList[i].pid = pid;
+            strncpy(backgroundProcessList[i].commandLine, commandLine, sizeof(backgroundProcessList[i].commandLine) - 1);
+            backgroundProcessList[i].commandLine[sizeof(backgroundProcessList[i].commandLine) - 1] = '\0';
+            break;
+        }
+    }
+}
+
+// Function to get the PID of a background process based on its job number
+pid_t getProcessPID(int jobNumber)
+{
+    for (int i = 0; i < MAX_HISTORY_SIZE; i++)
+    {
+        if (backgroundProcessList[i].jobNumber == jobNumber)
+        {
+            return backgroundProcessList[i].pid;
+        }
+    }
+    return 0; // Return 0 if no matching job number is found (indicating an error)
+}
+
+// Function to get the command line of a background process based on its job number
+const char *getProcessCommandLine(int jobNumber)
+{
+    for (int i = 0; i < MAX_HISTORY_SIZE; i++)
+    {
+        if (backgroundProcessList[i].jobNumber == jobNumber)
+        {
+            return backgroundProcessList[i].commandLine;
+        }
+    }
+    return NULL; // Return NULL if no matching job number is found (indicating an error)
+}
+
+bool isProcessActive(int jobNumber)
+{
+    // Get the PID of the background process based on its job number
+    pid_t pid = getProcessPID(jobNumber);
+
+    if (pid == 0)
+    {
+        // If no matching job number is found, consider the process inactive
+        return false;
+    }
+
+    // Check the status of the process using waitpid with WNOHANG
+    int status;
+    pid_t result = waitpid(pid, &status, WNOHANG);
+
+    if (result == -1)
+    {
+        // An error occurred while checking the process status
+        // You may want to handle this error case accordingly
+        return false;
+    }
+    else if (result == 0)
+    {
+        // The process is still running
+        return true;
+    }
+    else
+    {
+        // The process has terminated
+        return false;
+    }
+}
+
+void listActiveBackgroundProcesses()
+{
+    if (backgroundProcessesCount() == 0)
+    {
+        printf("No active background processes.\n");
+    }
+    else
+    {
+        printf("Active background processes:\n");
+        printActiveBackgroundProcesses();
+    }
+}
+
+int backgroundProcessesCount()
+{
+    int count = 0;
+
+    // Loop through your background processes and count them.
+    for (int i = 0; i < MAX_HISTORY_SIZE; i++)
+    {
+        // Check if the process with jobNumber 'i' is active (still running)
+        if (isProcessActive(i))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+void printActiveBackgroundProcesses()
+{
+    // Implement this function to print the active background processes
+    // in the required format "[Job number]+ [CMD's PID] [CMD's command line]".
+
+    // You can iterate through your backgroundProcesses data structure
+    // and print the information for each active process.
+
+    for (int i = 0; i < MAX_HISTORY_SIZE; i++)
+    {
+        // Check if the process with jobNumber 'i' is active (still running)
+        if (isProcessActive(i))
+        {
+            // Print the information for the active process in the required format.
+            printf("[%d]+ [%d] %s\n", i, getProcessPID(i), getProcessCommandLine(i));
+        }
+    }
+}
+
+
 
 char *get_input(void)
 {
@@ -516,4 +901,3 @@ void free_tokens(tokenlist *tokens)
 	free(tokens->items);
 	free(tokens);
 }
-
