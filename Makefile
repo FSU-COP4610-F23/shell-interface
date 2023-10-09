fe77f5ab103fa16f54782c28fba05f02e227bbf9
Jasmine Djama Masopeh <masopeh@linprog2.cs.fsu.edu>
2023-10-09 17:45:28 -0400

    latest updated 10/9/2023

--- Makefile
+++ Makefile
@@ -1,37 +1,118 @@
+# SRC := src
+# OBJ := obj
+# BIN := bin
+# EXECUTABLE := shell
+# MYTIMEOUT_EXECUTABLE := mytimeout
+
+# SRCS := $(wildcard $(SRC)/*.c)
+# OBJS := $(patsubst $(SRC)/%.c,$(OBJ)/%.o,$(SRCS))
+# INCS := -Iinclude/
+# DIRS := $(OBJ)/ $(BIN)/
+# EXEC := $(BIN)/$(EXECUTABLE)
+# MYTIMEOUT_EXEC := $(BIN)/$(MYTIMEOUT_EXECUTABLE)
+
+# CC := gcc
+# CFLAGS := -g -Wall -std=c99 $(INCS)
+# LDFLAGS :=
+
+# all: $(EXEC) $(MYTIMEOUT_EXEC)
+
+# $(EXEC): $(OBJS)
+# 	$(CC) $(CFLAGS) $(OBJS) -o $(EXEC)
+
+# $(MYTIMEOUT_EXEC): $(OBJ)/mytimeout/mytimeout.o $(OBJ)/lexer.o
+# 	$(CC) $(CFLAGS) $^ -o $(MYTIMEOUT_EXEC)
+
+# $(OBJ)/%.o: $(SRC)/%.c
+# 	$(CC) $(CFLAGS) -c $< -o $@
+
+# run: $(EXEC)
+# 	$(EXEC)
+
+# clean:
+# 	rm -f $(OBJ)/*.o $(EXEC) $(MYTIMEOUT_EXEC)
+
+# $(shell mkdir -p $(DIRS))
+
+# .PHONY: run clean all
+
+
+
+
+
+# SRC := src
+# OBJ := obj
+# BIN := bin
+# SHELL_EXECUTABLE := shell
+# MYTIMEOUT_EXECUTABLE := mytimeout
+
+# SRCS := $(wildcard $(SRC)/*.c)
+# OBJS := $(patsubst $(SRC)/%.c,$(OBJ)/%.o,$(SRCS))
+# INCS := -Iinclude/
+
+# CC := gcc
+# CFLAGS := -g -Wall -std=c99 $(INCS)
+
+# all: $(BIN)/$(SHELL_EXECUTABLE) $(BIN)/$(MYTIMEOUT_EXECUTABLE)
+
+# $(BIN)/$(SHELL_EXECUTABLE): $(OBJ)/lexer.o
+# 	$(CC) $(CFLAGS) $^ -o $@
+
+# $(BIN)/$(MYTIMEOUT_EXECUTABLE): $(OBJ)/mytimeout/mytimeout.o
+# 	$(CC) $(CFLAGS) $^ -o $@
+
+# $(OBJ)/mytimeout/mytimeout.o: $(SRC)/mytimeout/mytimeout.c
+# 	@mkdir -p $(OBJ)/mytimeout
+# 	$(CC) $(CFLAGS) -c $< -o $@
+
+# $(OBJ)/lexer.o: $(SRC)/lexer.c
+# 	$(CC) $(CFLAGS) -c $< -o $@
+
+# run: $(BIN)/$(SHELL_EXECUTABLE)
+# 	$<
+
+# # run-mytimeout: $(BIN)/$(MYTIMEOUT_EXECUTABLE)
+# # 	$<
+
+# clean:
+# 	rm -f $(OBJ)/*.o $(BIN)/$(SHELL_EXECUTABLE) $(BIN)/$(MYTIMEOUT_EXECUTABLE)
+
+# .PHONY: run-shell run-mytimeout clean all
+
+
 SRC := src
 OBJ := obj
 BIN := bin
-EXECUTABLE := shell
+SHELL_EXECUTABLE := shell
 MYTIMEOUT_EXECUTABLE := mytimeout
 
 SRCS := $(wildcard $(SRC)/*.c)
 OBJS := $(patsubst $(SRC)/%.c,$(OBJ)/%.o,$(SRCS))
 INCS := -Iinclude/
-DIRS := $(OBJ)/ $(BIN)/
-EXEC := $(BIN)/$(EXECUTABLE)
-MYTIMEOUT_EXEC := $(BIN)/$(MYTIMEOUT_EXECUTABLE)
 
 CC := gcc
 CFLAGS := -g -Wall -std=c99 $(INCS)
-LDFLAGS :=
 
-all: $(EXEC) $(MYTIMEOUT_EXEC)
+all: $(BIN)/$(SHELL_EXECUTABLE) $(MYTIMEOUT_EXECUTABLE)
 
-$(EXEC): $(OBJS)
-	$(CC) $(CFLAGS) $(OBJS) -o $(EXEC)
+$(BIN)/$(SHELL_EXECUTABLE): $(OBJS)
+	$(CC) $(CFLAGS) $^ -o $@
 
-$(MYTIMEOUT_EXEC): $(OBJ)/mytimeout/mytimeout.o $(OBJ)/lexer.o
-	$(CC) $(CFLAGS) $^ -o $(MYTIMEOUT_EXEC)
+$(MYTIMEOUT_EXECUTABLE): $(OBJ)/mytimeout/mytimeout.o
+	$(CC) $(CFLAGS) $^ -o $@
+
+
+$(OBJ)/mytimeout/mytimeout.o: $(SRC)/mytimeout/mytimeout.c
+	@mkdir -p $(OBJ)/mytimeout
+	$(CC) $(CFLAGS) -c $< -o $@
 
 $(OBJ)/%.o: $(SRC)/%.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
-run: $(EXEC)
-	$(EXEC)
+run: $(BIN)/$(SHELL_EXECUTABLE)
+	$<
 
 clean:
-	rm -f $(OBJ)/*.o $(EXEC) $(MYTIMEOUT_EXEC)
-
-$(shell mkdir -p $(DIRS))
+	rm -f $(OBJ)/*.o $(BIN)/$(SHELL_EXECUTABLE) $(MYTIMEOUT_EXECUTABLE)  # Remove from root directory
 
 .PHONY: run clean all
--- src/mytimeout/mytimeout.c
+++ src/mytimeout/mytimeout.c
@@ -7,66 +7,13 @@
 #include <signal.h> // Include this header for kill and SIGTERM
 #include "lexer.h" 
 
-// int main(int argc, char *argv[]) {
-//     if (argc < 3) {
-//         fprintf(stderr, "Usage: %s [seconds] [command] [command-args]\n", argv[0]);
-//         return 1;
-//     }
-
-//     int timeout_seconds = atoi(argv[1]);
-//     if (timeout_seconds <= 0) {
-//         fprintf(stderr, "Invalid timeout value\n");
-//         return 1;
-//     }
-
-//     pid_t child_pid = fork();
-
-//     if (child_pid == -1) {
-//         perror("fork");
-//         return 1;
-//     }
-
-//     if (child_pid == 0) {
-//         // This code runs in the child process
-//         char *cmd = argv[2];
-//         char **cmd_args = &argv[2];
-
-//         execvp(cmd, cmd_args);
-//         perror("execvp"); // If execvp fails, print an error
-//         exit(1);
-//     } else {
-//         // This code runs in the parent process
-//         sleep(timeout_seconds);
-
-//         // Try to terminate the child process gracefully
-//         int status;
-//         if (waitpid(child_pid, &status, WNOHANG) == 0) {
-//             // Child process is still running; send a termination signal
-//             kill(child_pid, SIGTERM);
-//         }
-
-//         // Wait for the child process to finish
-//         waitpid(child_pid, &status, 0);
-
-//         if (WIFEXITED(status)) {
-//             return WEXITSTATUS(status);
-//         } else {
-//             fprintf(stderr, "Child process terminated abnormally\n");
-//             return 1;
-//         }
-//     }
-
-//     return 0;
-// }
-
-
-int execute_with_timeout(tokenlist *tokens, int timeout_seconds) {
-    if (tokens->size < 2) {
-        fprintf(stderr, "Usage: %s [seconds] [command] [command-args]\n", tokens->items[0]);
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s [seconds] [command] [command-args]\n", argv[0]);
         return 1;
     }
 
-    timeout_seconds = atoi(tokens->items[1]);
+    int timeout_seconds = atoi(argv[1]);
     if (timeout_seconds <= 0) {
         fprintf(stderr, "Invalid timeout value\n");
         return 1;
@@ -81,8 +28,8 @@ int execute_with_timeout(tokenlist *tokens, int timeout_seconds) {
 
     if (child_pid == 0) {
         // This code runs in the child process
-        char *cmd = tokens->items[2];
-        char **cmd_args = &(tokens->items[2]);
+        char *cmd = argv[2];
+        char **cmd_args = &argv[2];
 
         execvp(cmd, cmd_args);
         perror("execvp"); // If execvp fails, print an error
@@ -108,6 +55,59 @@ int execute_with_timeout(tokenlist *tokens, int timeout_seconds) {
             return 1;
         }
     }
-                                       
+
     return 0;
 }
+
+
+// int execute_with_timeout(tokenlist *tokens, int timeout_seconds) {
+//     if (tokens->size < 2) {
+//         fprintf(stderr, "Usage: %s [seconds] [command] [command-args]\n", tokens->items[0]);
+//         return 1;
+//     }
+
+//     timeout_seconds = atoi(tokens->items[1]);
+//     if (timeout_seconds <= 0) {
+//         fprintf(stderr, "Invalid timeout value\n");
+//         return 1;
+//     }
+
+//     pid_t child_pid = fork();
+
+//     if (child_pid == -1) {
+//         perror("fork");
+//         return 1;
+//     }
+
+//     if (child_pid == 0) {
+//         // This code runs in the child process
+//         char *cmd = tokens->items[2];
+//         char **cmd_args = &(tokens->items[2]);
+
+//         execvp(cmd, cmd_args);
+//         perror("execvp"); // If execvp fails, print an error
+//         exit(1);
+//     } else {
+//         // This code runs in the parent process
+//         sleep(timeout_seconds);
+
+//         // Try to terminate the child process gracefully
+//         int status;
+//         if (waitpid(child_pid, &status, WNOHANG) == 0) {
+//             // Child process is still running; send a termination signal
+//             kill(child_pid, SIGTERM);
+//         }
+
+//         // Wait for the child process to finish
+//         waitpid(child_pid, &status, 0);
+
+//         if (WIFEXITED(status)) {
+//             return WEXITSTATUS(status);
+//         } else {
+//             fprintf(stderr, "Child process terminated abnormally\n");
+//             return 1;
+//         }
+//     }
+                                       
+//     return 0;
+// }
